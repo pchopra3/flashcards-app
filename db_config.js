de22aca0372591cace71b0437d5fae5f4a3dfdf0
pchopra3 <51158877+pchopra3@users.noreply.github.com>
2019-06-18 16:58:16 +0530

    Update db_config.js
    api key and other details of the firebase project taken off

--- db_config.js
+++ db_config.js
@@ -1,9 +1,2 @@
 export const DB_CONFIG = {
-   apiKey: "AIzaSyC98z-WCqpMqZ_8g1_Ospq7q550EJUmVNA",
-   authDomain: "myproject5-81112.firebaseapp.com",
-   databaseURL: "https://myproject5-81112.firebaseio.com",
-   projectId: "myproject5-81112",
-   storageBucket: "myproject5-81112.appspot.com",
-   messagingSenderId: "896341655598",
-   appId: "1:896341655598:web:3dd5f22980cadaa4"
 }
